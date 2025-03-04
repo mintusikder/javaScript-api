2a5de00c2f959b8aaca3f85b96d524cd7bfa8dcb
Mintu Sikder <mintusikder15@gmail.com>
2025-03-04 21:27:57 +0600

    added keys values seal

--- basic-es6/object.js
+++ basic-es6/object.js
@@ -3,6 +3,10 @@ const person ={
     age : 33,
     country: "BD"
 }
+Object.seal(person)
+Object.freeze(person)
+person.status = "Not Found"
+person.age = 20
 
 console.log(Object.keys(person))
 console.log(Object.values(person))
