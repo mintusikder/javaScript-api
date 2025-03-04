415f58f98bcc04e747044ac04b4d000699d51a9e
Mintu Sikder <mintusikder15@gmail.com>
2025-03-04 21:25:26 +0600

    added javaScript api

---
+++ basic-es6/object.js
@@ -0,0 +1,9 @@
+const person ={
+    name : "Mintu",
+    age : 33,
+    country: "BD"
+}
+
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
