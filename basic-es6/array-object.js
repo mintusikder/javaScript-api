3f8d07a0735a963f9e3a99aee87f5ce94371f919
Mintu Sikder <mintusikder15@gmail.com>
2025-03-04 22:34:14 +0600

    added array-boject

---
+++ basic-es6/array-object.js
@@ -0,0 +1,14 @@
+const person ={
+    name : "Mintu",
+    age : 33,
+    country: "BD"
+}
+
+const {name,age} = person 
+// console.log(age)
+
+const people = ["mintu", "rakib","sakib"]
+
+
+const [d,f] = people
+console.log(f)
