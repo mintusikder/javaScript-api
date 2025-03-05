50684026996486da7fc88ecfe273ecca00c44f2b
Mintu Sikder <mintusikder15@gmail.com>
2025-03-05 21:04:43 +0600

    added more es6

---
+++ more-es6/es6.js
@@ -0,0 +1,15 @@
+const person ={
+    name : "Mintu",
+    age: 33,
+    friend : ["sadhin", "sajib"],
+    details:{
+        job: "yes",
+        isMarried : true ,
+        status : "Not found",
+        father: {
+            name:"rakib"
+        }
+
+    }
+}
+console.log(person.details.father.name)
